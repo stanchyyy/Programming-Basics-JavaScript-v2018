76fc06dca8d1de8ae4ce5bb6aae559d1a020edf2
stanchyyy <stanchyyy@gmail.com>
2022-03-17 02:08:04 +0200

    solved metric converter

--- Chapter 3.1. Simple Conditions/08. Metric Converter.js
+++ Chapter 3.1. Simple Conditions/08. Metric Converter.js
@@ -1,8 +1,8 @@
 function MetricConverter([inputLenght,inputUnit,outputUnit]){
-    let lenght = parseInt(inputLenght);
+    let lenght = parseFloat(inputLenght);
     let outUnit = outputUnit.toLowerCase();
     let inUnit = inputUnit.toLowerCase();
-    //convert in unit to meters
+    //convert IN unit to meters
     if(inUnit==="mm"){
         lenght/=1000;
     }
@@ -13,18 +13,17 @@ function MetricConverter([inputLenght,inputUnit,outputUnit]){
         lenght*=1609.344;
     }
     if(inUnit==="in"){
-        lenght*=39.3700787;
+        lenght*= 0.0254;
     }
     if(inUnit==="km"){
         lenght*=1000;
     }
     if(inUnit==="ft"){
-        lenght*=3.2808399;
+        lenght/=3.2808;
     }
     if(inUnit==="yd"){
-        lenght*=0.9144;
+        lenght*= 0.9144;
     }
-    //convert meters to out unit
     if(outUnit==="mm"){
         lenght*=1000;
     }
@@ -32,25 +31,39 @@ function MetricConverter([inputLenght,inputUnit,outputUnit]){
         lenght*=100;
     }
     if(outUnit==="mi"){
-        lenght/=0.0006213712;
+        lenght*=0.000621371192;
     }
     if(outUnit==="in"){
         lenght*=39.3700787;
     }
     if(outUnit==="km"){
-        lenght/=1000;
+        lenght*=0.001;
     }
     if(outUnit==="ft"){
         lenght*=3.2808399;
     }
     if(outUnit==="yd"){
-        lenght/=1.0936133;
+        lenght*= 1.0936133;
     }
     console.log(lenght+" "+ outUnit)
 
 }
 
-MetricConverter([450, "yd", "km"]);
-MetricConverter([12, "km", "ft"]);
-MetricConverter([150, "mi", "in"]);
-  
+MetricConverter([1, "m", "m"]);
+MetricConverter([1, "m", "mm"]);
+MetricConverter([1, "m", "cm"]);
+MetricConverter([1, "m", "mi"]);
+MetricConverter([1, "m", "in"]);
+MetricConverter([1, "m", "km"]);
+MetricConverter([1, "m", "ft"]);
+MetricConverter([1, "m", "yd"]);
+
+
+
+MetricConverter([1, "mm", "m"]);
+MetricConverter([1, "cm", "m"]);
+MetricConverter([1, "mi", "m"]);
+MetricConverter([1, "in", "m"]);
+MetricConverter([1, "km", "m"]);
+MetricConverter([1, "ft", "m"]);
+MetricConverter([1, "yd", "m"]);
